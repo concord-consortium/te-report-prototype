16cf9f17b54f88cda95e3f72547cd2c8747eb5d5
dalove1025 <dalove1025@gmail.com>
2019-06-17 14:12:10 -0400

    Another test deployment to Heroku.

--- server.ts
+++ server.ts
@@ -14,7 +14,11 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   // DAVE TODO: import report from main module and pass request parameters to it along with response (so you can stream the output)
   // for now just output the post parameters to ensure you are getting them from the user report
-  res.send(`<html><body><div>POST params:</div><div><pre>${JSON.stringify(req.body, null, 2)}</pre></div></body></html>`);
+  // Might make sense to look at req.body.format === "csv" to see what format we want, but for now, we won't grab that
+  // from the form and just assume it's a csv output.
+  res.setHeader('Content-disposition', 'attachment; filename="portal-report-' + Date.now() + '.csv');
+  res.sent("col1,col2\n43,1234");
+  // res.send(`<html><body><div>POST params:</div><div><pre>${JSON.stringify(req.body, null, 2)}</pre></div></body></html>`);
 });
 
 app.listen(PORT, () => {
